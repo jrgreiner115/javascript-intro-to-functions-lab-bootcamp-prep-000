12255f01e21d22672cb32d703d40339ab333f1de
Joe Greiner <jrgreiner115@gmail.com>
2018-02-09 22:15:35 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,11 @@
+function shout(string) {return string.toUpperCase()
+}
+function whisper(string) {return string.toLowerCase()
+}
+function logWhisper() {console.log("hello")
+}
+function logShout() {console.log("HELLO")
+}
+function sayHiToGrandma(string) {
+  if string.
+}
